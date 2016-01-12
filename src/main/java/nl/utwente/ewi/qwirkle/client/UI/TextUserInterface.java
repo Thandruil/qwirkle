2ea64442fd439823cd1d7df6fcc41eab93209789
Jasper <jasper.boot@itres.nl>
2016-01-12 17:36:39 +0100

    Adds basic functionality to the TUI

--- src/main/java/nl/utwente/ewi/qwirkle/client/UI/TextUserInterface.java
+++ src/main/java/nl/utwente/ewi/qwirkle/client/UI/TextUserInterface.java
@@ -4,16 +4,45 @@ import nl.utwente.ewi.qwirkle.client.Client;
 import nl.utwente.ewi.qwirkle.model.Player;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class TextUserInterface implements IUserInterface {
 
+    Scanner scanner;
+
+    public TextUserInterface() {
+        scanner = new Scanner(System.in);
+    }
+
     @Override
     public void init() {
-
+        clear();
+        System.out.println("TUI successfully initialized.");
     }
 
     @Override
     public Client.GameType selectGameType() {
+        int choice = -1;
+        do {
+            clear();
+            System.out.println("Welcome to Qwirkle!");
+            System.out.println("");
+            System.out.println("1 = Play local against AI or your friends");
+            System.out.println("2 = Play online against friends or strangers");
+            System.out.println("");
+            System.out.println("0 = Exit game");
+            System.out.println("");
+            choice = scanner.nextInt();
+        } while (choice < 0 || choice > 2);
+        if (choice == 1) {
+            return Client.GameType.LOCAL;
+        }
+        if (choice == 2) {
+            return Client.GameType.ONLINE;
+        }
+        if (choice == 0) {
+            exit();
+        }
         return null;
     }
 
@@ -28,5 +57,15 @@ public class TextUserInterface implements IUserInterface {
     @Override
     public void gameOver() {
 
+    }
+
+    public void exit() {
+        System.exit(0);
+    }
+
+    private void clear() {
+        for(int i = 0; i < 20; i++) {
+            System.out.println("");
+        }
     }
 }
