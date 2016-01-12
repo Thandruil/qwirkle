a711e8c208024377501bad3c7ed91e1926e0a4e8
Jasper <jasper.boot@itres.nl>
2016-01-12 12:06:43 +0100

    Adds basis for client

--- src/main/java/nl/utwente/ewi/qwirkle/client/Client.java
+++ src/main/java/nl/utwente/ewi/qwirkle/client/Client.java
@@ -1,8 +1,25 @@
 package nl.utwente.ewi.qwirkle.client;
 
+import nl.utwente.ewi.qwirkle.client.UI.IUserInterface;
+import nl.utwente.ewi.qwirkle.client.UI.TextUserInterface;
+import nl.utwente.ewi.qwirkle.model.Player;
+import nl.utwente.ewi.qwirkle.model.PlayerAmountInvalidException;
+import nl.utwente.ewi.qwirkle.util.Logger;
+
+import java.util.Set;
+
 public class Client implements Runnable {
 
+    public enum GAME_TYPE {
+        LOCAL,
+        ONLINE
+    }
+
     boolean running = true;
+
+    GameController game;
+
+    IUserInterface ui;
 
     @Override
     public void run() {
@@ -11,11 +28,32 @@ public class Client implements Runnable {
     }
 
     void init() {
-
+        ui = new TextUserInterface();
+        Logger.info("Client initialized.");
     }
 
     void loop() {
-
+        switch(ui.selectGameType()) {
+            case LOCAL:
+                Set<Player> playerSet = ui.selectPlayers();
+                while (playerSet.size() >= 2 && playerSet.size() <= 4) {
+                    playerSet = ui.selectPlayers();
+                }
+                try {
+                    game = new GameController(playerSet);
+                } catch (PlayerAmountInvalidException e) {
+                    Logger.error("PlayerAmountInvalidException occoured.", e);
+                }
+                break;
+            case ONLINE:
+                // Wacht op keuze UI: Server en port
+                // Check op server
+                // Wacht op keuze UI: Gebruikersnaam en speler type
+                // Check op gebruikersnaam
+                // Wacht op game
+                // Start game
+                break;
+        }
     }
 
     void stop() {
@@ -23,6 +61,7 @@ public class Client implements Runnable {
     }
 
     public static void main(String[] args) {
+        Logger.info("Starting client . . .");
         Client client = new Client();
         new Thread(client).start();
     }
