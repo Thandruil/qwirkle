663301c62cc83e562c065a6ece7c72ef7939a8ef
Jasper <jasper.boot@itres.nl>
2016-01-12 12:07:18 +0100

    Adds coordinate model

---
+++ src/main/java/nl/utwente/ewi/qwirkle/model/Coordinate.java
@@ -0,0 +1,23 @@
+package nl.utwente.ewi.qwirkle.model;
+
+public class Coordinate {
+    private int x;
+    private int y;
+
+    public Coordinate(int x, int y) {
+        setXY(x, y);
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public void setXY(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
