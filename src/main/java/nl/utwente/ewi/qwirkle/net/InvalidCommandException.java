ed9e9e6bdeef09f4ee119002e25fc7302a13954e
Jasper <jasper.boot@itres.nl>
2016-01-13 14:45:41 +0100

    Fixes error in InvalidCommandException

--- src/main/java/nl/utwente/ewi/qwirkle/net/InvalidCommandException.java
+++ src/main/java/nl/utwente/ewi/qwirkle/net/InvalidCommandException.java
@@ -2,6 +2,6 @@ package nl.utwente.ewi.qwirkle.net;
 
 public class InvalidCommandException extends ProtocolException {
     public InvalidCommandException() {
-        super(IProtocol.Error.COMMAND_NOT_FOUND);
+        super(IProtocol.Error.INVALID_COMMAND);
     }
 }
