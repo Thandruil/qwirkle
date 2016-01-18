40f9f1b7f87a802d1535da8489cb83f8bb8c3f73
Erik Gaal <git@erikgaal.nl>
2016-01-18 20:57:40 +0100

    Updated ClientHandler with states and basic identify and chat functionality

--- src/main/java/nl/utwente/ewi/qwirkle/server/ClientHandler.java
+++ src/main/java/nl/utwente/ewi/qwirkle/server/ClientHandler.java
@@ -1,25 +1,42 @@
 package nl.utwente.ewi.qwirkle.server;
 
-import nl.utwente.ewi.qwirkle.net.Protocol;
+import nl.utwente.ewi.qwirkle.net.IProtocol;
+import nl.utwente.ewi.qwirkle.net.IllegalStateException;
 import nl.utwente.ewi.qwirkle.net.ProtocolException;
+import nl.utwente.ewi.qwirkle.net.ServerProtocol;
+import nl.utwente.ewi.qwirkle.server.packet.IPacket;
 import nl.utwente.ewi.qwirkle.util.Logger;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.List;
 
 public class ClientHandler implements Runnable {
 
-    private Socket socket;
-    private boolean closed;
-    private String name;
+    public enum ClientState {
+        CONNECTED,
+        IDENTIFIED,
+        QUEUED,
+        GAME_WAITING,
+        GAME_TURN,
+    }
 
+    private Socket socket;
     private BufferedReader in;
     private BufferedWriter out;
+
+    private boolean closed;
+
+    private ClientState state;
+
+    private String name;
+    private List<IProtocol.Feature> features;
 
     public ClientHandler(Socket socket) throws IOException {
         this.socket = socket;
         this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         this.out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        this.state = ClientState.CONNECTED;
         this.closed = false;
     }
 
@@ -27,10 +44,52 @@ public class ClientHandler implements Runnable {
         return String.valueOf(name == null ? socket.getInetAddress() : name);
     }
 
+    public List<IProtocol.Feature> getFeatures() {
+        return features;
+    }
+
+    public ClientState getState() {
+        return state;
+    }
+
+    public void identify(String name, List<IProtocol.Feature> features) throws IllegalStateException, NameException {
+        if (state != ClientState.CONNECTED) throw new IllegalStateException();
+        PlayerList.addPlayer(name, this);
+        this.name = name;
+        this.features = features;
+        this.state = ClientState.IDENTIFIED;
+        Logger.info(String.format("Client %s identified as %s", socket.getInetAddress(), getName()));
+        writePacket(ServerProtocol.identify());
+    }
+
+    public void queue() throws IllegalStateException {
+        if (state != ClientState.IDENTIFIED && state != ClientState.QUEUED) throw new IllegalStateException();
+    }
+
+    public void chat(String recipient, String message) throws IllegalStateException {
+        if (state == ClientState.CONNECTED) throw new IllegalStateException();
+        if (recipient.equals("global"))
+            PlayerList.getPlayerList().values().stream()
+                    .filter(p -> p.getFeatures().contains(IProtocol.Feature.CHAT))
+                    .filter(p -> p.getState() != ClientState.CONNECTED)
+                    .forEach(p -> p.writePacket(ServerProtocol.chat(recipient, getName(), message)));
+        if (recipient.equals("game") && state == ClientState.GAME_TURN || state == ClientState.GAME_WAITING)
+            PlayerList.getPlayerList().values().stream()
+                    .filter(p -> p.getFeatures().contains(IProtocol.Feature.CHAT))
+                    .filter(p -> p.getState() != ClientState.CONNECTED)
+                    .forEach(p -> p.writePacket(ServerProtocol.chat(recipient, getName(), message)));
+        if (recipient.startsWith("@")) {
+            ClientHandler client;
+            if ((client = PlayerList.getPlayerList().get(recipient.substring(1))) != null && client.getFeatures().contains(IProtocol.Feature.CHAT))
+                client.writePacket(ServerProtocol.chat(recipient, getName(), message));
+        }
+    }
+
     public void run() {
         while (!closed) {
             try {
                 String packet = in.readLine();
+                if (packet == null) disconnect();
                 Logger.debug(String.format("[<-%s] %s", getName(), packet));
                 parsePacket(packet);
             } catch (IOException e) {
@@ -43,6 +102,7 @@ public class ClientHandler implements Runnable {
     public void disconnect() {
         if (closed) return;
         Logger.info(String.format("Disconnecting %s", getName()));
+        PlayerList.removePlayer(name);
         try {
             in.close();
         } catch (IOException e) {
@@ -71,15 +131,17 @@ public class ClientHandler implements Runnable {
         }
     }
 
-    private void parsePacket(String packet) {
+    private void parsePacket(String message) {
         try {
-            if (packet == null) {
-                disconnect();
-            } else {
-                Protocol.parsePacket(packet);
-            }
+            IPacket parsedPacket = ServerProtocol.parsePacket(message);
+            parsedPacket.process(this);
         } catch (ProtocolException e) {
-            writePacket(Protocol.error(e.getError()));
+            Logger.fatal(e);
+            writePacket(ServerProtocol.error(e.getError()));
+            disconnect();
+        } catch (QwirkleException e) {
+            Logger.error(e);
+            writePacket(ServerProtocol.error(e.getError()));
         }
     }
 }
