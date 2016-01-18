2f23cdf2f9dbb711cc96c4192ffdd8f97853bee0
Erik Gaal <git@erikgaal.nl>
2016-01-18 20:52:48 +0100

    Protocol implmentation specific for Server with packet models

---
+++ src/main/java/nl/utwente/ewi/qwirkle/server/packet/QueuePacket.java
@@ -0,0 +1,19 @@
+package nl.utwente.ewi.qwirkle.server.packet;
+
+import nl.utwente.ewi.qwirkle.net.IllegalStateException;
+import nl.utwente.ewi.qwirkle.server.ClientHandler;
+import nl.utwente.ewi.qwirkle.server.QwirkleException;
+
+import java.util.List;
+
+public class QueuePacket implements IPacket {
+    List<Integer> queues;
+
+    public QueuePacket(List<Integer> queues) {
+        this.queues = queues;
+    }
+
+    public void process(ClientHandler client) throws QwirkleException, IllegalStateException {
+
+    }
+}
